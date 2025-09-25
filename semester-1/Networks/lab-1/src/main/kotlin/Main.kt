b004fce5cb6c70621a56e72978baf2401fece424
Igor Nechaev <i.nechaev@lipt-soft.ru>
2025-09-26 01:41:02 +0300

    Networks lab-1

---
+++ semester-1/Networks/lab-1/src/main/kotlin/Main.kt
@@ -0,0 +1,97 @@
+package com.example.exmple
+
+fun main() {
+    val n = readln().toInt()
+    val messages = readln().split(" ")
+    labFirst(n, messages).forEach { receivedMessage ->
+        println(receivedMessage)
+    }
+}
+
+fun labFirst(n: Int, messages: List<String>): List<String> {
+    val walshCodes = getWalshCodes(n)
+    val baseStations = buildList {
+        repeat(n) { index ->
+            add(
+                BaseStation(
+                    message = messages[index],
+                    code = walshCodes[index]
+                )
+            )
+        }
+    }
+
+    val signal = baseStations.map { baseStation ->
+        baseStation.message.toBits().toPlusMinus().applyCode(baseStation.code)
+    }.multiplex()
+
+    return buildList {
+        baseStations.forEach { baseStation ->
+            val message = signal.decodeBits(baseStation.code).decodeToString()
+            add(message)
+        }
+    }
+}
+
+class BaseStation(
+    val message: String,
+    val code: IntArray,
+)
+
+fun String.toBits(): IntArray =
+    flatMap { char ->
+        char.code.toString(2)
+            .padStart(8, '0')
+            .map { char -> char.digitToInt() }
+    }.toIntArray()
+
+fun IntArray.toPlusMinus(): IntArray = map { value ->
+    when (value) {
+        0 -> -1
+        else -> 1
+    }
+}.toIntArray()
+
+fun IntArray.applyCode(code: IntArray): IntArray =
+    flatMap { bit -> code.map { codeBit -> bit * codeBit } }.toIntArray()
+
+fun List<IntArray>.multiplex(): IntArray =
+    reduce { acc, signal -> acc.zip(signal) { a, b -> a + b }.toIntArray() }
+
+fun IntArray.decodeBits(code: IntArray): IntArray {
+    return toList()
+        .chunked(code.size) { chunk ->
+            val correlation = chunk.indices.sumOf { j -> chunk[j] * code[j] }
+            when (correlation >= 0) {
+                true -> 1
+                false -> 0
+            }
+        }
+        .toIntArray()
+}
+
+fun IntArray.decodeToString(): String {
+    return toList()
+        .chunked(8) { chunk ->
+            chunk.fold(0) { acc, bit -> (acc shl 1) or bit }.toChar()
+        }
+        .joinToString("")
+}
+
+fun getWalshCodes(n: Int): Array<IntArray> {
+    val hadamard = Array(n) { IntArray(n) }
+    hadamard[0][0] = 1
+    var k = 1
+    while (k < n) {
+        for (i in 0..<k) {
+            for (j in 0..<k) {
+                hadamard[i + k][j] = hadamard[i][j]
+                hadamard[i][j + k] = hadamard[i][j]
+                hadamard[i + k][j + k] = -hadamard[i][j]
+            }
+        }
+        k += k
+    }
+
+    return hadamard
+}
---
+++ semester-1/Networks/lab-1/src/test/kotlin/LabFirstTest.kt
@@ -0,0 +1,29 @@
+import com.example.exmple.labFirst
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LabFirstTest {
+
+    @Test
+    fun labFirstTest() {
+        val messages = listOf(
+            listOf("DOG", "CAT", "HAM", "SUN"),
+            listOf("SUN", "HAM", "CAT", "DOG"),
+        )
+        val expected = listOf(
+            listOf("DOG", "CAT", "HAM", "SUN"),
+            listOf("SUN", "HAM", "CAT", "DOG"),
+        )
+
+        val receivedMessages = messages.map { messages ->
+            labFirst(
+                n = messages.size,
+                messages = messages
+            )
+        }
+
+        expected.forEachIndexed { index, expected ->
+            assertEquals(expected, receivedMessages[index])
+        }
+    }
+}
